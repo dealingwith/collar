1395cad1f4aa83d284f02628bc2c0ee1fbb33f65
Daniel Miller <dealingwith@gmail.com>
2019-11-03 23:07:57 -0600

    update Collar
    make it work again!

--- collar.js
+++ collar.js
@@ -1,8 +1,3 @@
-$(function () {
-  new MutationObserver(function (mutations) { collarTrello(); }).observe(document.querySelector('body'), { attributes: true });
-  collarTrello();
-});
-
 function collarTrello() {
   if ($('.collar-button').length) return;
 
@@ -12,3 +7,21 @@ function collarTrello() {
     $(this).closest('.list-wrapper').toggleClass('collared');
   });
 }
+
+function keepCollars() {
+  const targetNode = document.getElementById('board');
+  if (!targetNode) {
+    // The node does not exist yet. Wait 500ms and try again.
+    window.setTimeout(keepCollars, 500);
+    return;
+  }
+  const observer = new MutationObserver(collarTrello);
+  observer.observe(targetNode, {
+    childList: true
+  });
+}
+
+$(function () {
+  collarTrello();
+  keepCollars();
+});
